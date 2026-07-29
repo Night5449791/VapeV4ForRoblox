d0f85760af02ac9fb2f842f9820c2741f06b1964
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-07-30 06:15:20 +0800

    push

--- src/games/universal - base/Utility/CheaterDetector.lua
+++ src/games/universal - base/Utility/CheaterDetector.lua
@@ -4,24 +4,13 @@ local CheaterDetector
 local Mode
 local Profile
 local Users
-local Group
-local Role
-
-local cIds = {
-	[2237398751] = true,
-	[2030419772] = true,
-}
 
 local cUsernames = {
 	["LaylaPowerGalaxy"] = true,
 }
 
 local function playerAdded(plr)
-	
-	local reason = (Users and table.find(Users.ListEnabled, tostring(plr.UserId))) and 'blacklisted_user'
-		or cIds[plr.UserId] and 'cheater_userid'
-		or cUsernames[plr.Name] and 'cheater_username'
-		
+	local reason = (Users and table.find(Users.ListEnabled, tostring(plr.UserId))) or cIds[plr.UserId] or cUsernames[plr.Name]	
 	if reason then
 		notif('CheaterDetector', 'Cheater Detected ('..reason..'): '..plr.Name, 60, 'alert')
 		whitelist.customtags[plr.Name] = {{text = 'CHEATER', color = Color3.new(1, 0, 0)}}
