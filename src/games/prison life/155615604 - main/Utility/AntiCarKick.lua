07baca5da73fac62425b19f2269870cf43f9c0c1
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-07-30 10:04:30 +0800

    added anti kick

---
+++ src/games/prison life/155615604 - main/Utility/AntiCarKick.lua
@@ -0,0 +1,15 @@
+local AntiCarKick
+
+AntiCarKick = vape.Categories.Utility:CreateModule({
+	Name = 'AntiCarKick',
+	Function = function(callback)
+		if callback then
+			if game.CarContainer then
+                game.CarContainer:Destroy()
+            else
+                return nil
+            end
+		end
+	end,
+	Tooltip = 'prevent those niggas from kicking u',
+})
