b5e059f16f9e373237c27f5bdc68bb7ebd7fcac5
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-07-31 02:58:20 +0800

    fixed typo

--- src/games/prison life/155615604 - main/Utility/AntiCarKick.lua
+++ src/games/prison life/155615604 - main/Utility/AntiCarKick.lua
@@ -4,10 +4,8 @@ AntiCarKick = vape.Categories.Utility:CreateModule({
 	Name = 'AntiCarKick',
 	Function = function(callback)
 		if callback then
-			if game.CarContainers then
-                game.CarContainers:Destroy()
-            else
-                return nil
+			if game.CarContainer then
+                game.CarContainer:Destroy()
             end
 		end
 	end,
