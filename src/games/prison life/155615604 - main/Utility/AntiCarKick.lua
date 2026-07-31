978db7580929aa886aae035d6762f30ce215cd2b
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-07-31 09:07:14 +0800

    pushed ack

--- src/games/prison life/155615604 - main/Utility/AntiCarKick.lua
+++ src/games/prison life/155615604 - main/Utility/AntiCarKick.lua
@@ -7,6 +7,8 @@ AntiCarKick = vape.Categories.Utility:CreateModule({
 			if game.Workspace.CarContainer then
                 game.Workspace.CarContainer:Destroy()
             end
+			notif("Force removed cars, rejoin to see cars again.")
+			AntiCarKick:Toggle()
 		end
 	end,
 	Tooltip = '(ONLY USE WHEN TARGETED FLING) prevent those niggas from kicking u',
