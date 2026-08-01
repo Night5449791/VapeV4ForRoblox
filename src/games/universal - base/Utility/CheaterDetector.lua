21f0c41742cb94c5cfe7db0c1c461af2464f2f9b
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-01 12:04:48 +0800

    upd

--- src/games/universal - base/Utility/CheaterDetector.lua
+++ src/games/universal - base/Utility/CheaterDetector.lua
@@ -26,6 +26,9 @@ local cUsernames = {
 	['BestCode_BaconThx'] = 'fling attempt, worst communist>',
 }
 
+
+notif('Loaded cheater DB,' os.date())
+
 local function playerAdded(plr)
 	local reason = (Users and table.find(Users.ListEnabled, tostring(plr.UserId))) or cUsernames[plr.Name]	
 	if reason then
