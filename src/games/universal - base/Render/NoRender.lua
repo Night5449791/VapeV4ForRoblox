a50aa1a1c51f13a97c34512f2bd037be791fc798
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-01 09:06:27 +0800

    push upd

--- src/games/universal - base/Render/NoRender.lua
+++ src/games/universal - base/Render/NoRender.lua
@@ -4,7 +4,11 @@ NoRender = vape.Categories.Render:CreateModule({
 	Name = 'NoRender',
 	Function = function(callback)
 		if callback then
-            game.RunService:Set3dRenderingEnabled(false)
+			if callback == true then
+				game.RunService:Set3dRenderingEnabled(false)
+			else
+            	game.RunService:Set3dRenderingEnabled(callback)
+			end
 		end
 	end,
 	Tooltip = 'disable rendering'
