1f372ce045f59236bf981677a9331d6add2e36e2
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-01 12:35:01 +0800

    push upd

---
+++ src/games/universal - base/Utility/Jerking.lua
@@ -0,0 +1,11 @@
+local Jerking
+
+Jerking = vape.Categories.Utility:CreateModule({
+	Name = 'Jerking',
+	Function = function(callback)
+		if callback then
+			-- code here
+		end
+	end,
+	Tooltip = 'we all know this lol',
+})
