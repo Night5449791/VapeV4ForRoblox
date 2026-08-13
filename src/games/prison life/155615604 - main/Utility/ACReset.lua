b34bb94225b75660e12eb9e88675e0691147a0d9
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-13 20:21:30 +0800

    printing shits

--- src/games/prison life/155615604 - main/Utility/ACReset.lua
+++ src/games/prison life/155615604 - main/Utility/ACReset.lua
@@ -17,8 +17,7 @@ ACReset = vape.Categories.Utility:CreateModule({
             if humanoid then
                 humanoid.Died:Connect(function()
                     if t then
-                        reqteam:InvokeServer("Neutral", 1)
-                        reqteam:InvokeServer(t, 1)
+                        notif("ACReset", "You have died, switching team to " .. t.Name)
                     end
                 end)
             end
