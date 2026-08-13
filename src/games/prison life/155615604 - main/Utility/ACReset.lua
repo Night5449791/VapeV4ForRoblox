e1618afcef8f9cb26506d3a29a20af257f1400d7
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-14 03:39:00 +0800

    fix shit that crash

--- src/games/prison life/155615604 - main/Utility/ACReset.lua
+++ src/games/prison life/155615604 - main/Utility/ACReset.lua
@@ -5,14 +5,14 @@ ACReset = vape.Categories.Utility:CreateModule({
     Name = 'ACReset',
     Function = function(callback)
         if callback then
-            repeat
+            entitylib.isAlive:Connect(function()
                 if not entitylib.isAlive then
                     local team = game:GetService("Players").LocalPlayer.Team
                     reqteam:InvokeServer("Neutral")
                     wait(.5)
                     reqteam:InvokeServer(team)
                 end
-            until not callback
+            end)
         end
     end,
     Tooltip = 'Automatically switch team when lplr is dead.'
