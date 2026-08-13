c1a6c481a4258d348154addfd234d97054e0e9a5
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-14 03:54:17 +0800

    deqwihufjphwqn

--- src/games/prison life/155615604 - main/Utility/ACReset.lua
+++ src/games/prison life/155615604 - main/Utility/ACReset.lua
@@ -5,15 +5,11 @@ ACReset = vape.Categories.Utility:CreateModule({
     Name = 'ACReset',
     Function = function(callback)
         if callback then
-            if entitylib.isAlive then
-                print('dev mode')
-            end
-                if not entitylib.isAlive then
-                    local team = game:GetService("Players").LocalPlayer.Team
-                    reqteam:InvokeServer("Neutral")
-                    wait(.5)
-                    reqteam:InvokeServer(team)
-                end
+            if not entitylib.isAlive then
+                local team = game:GetService("Players").LocalPlayer.Team
+                reqteam:InvokeServer("Neutral")
+                wait(.5)
+                reqteam:InvokeServer(team)
             end
         end
     end,
