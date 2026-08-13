8319c7584ab3213ef4bd8e5e2da3b65719be85c4
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-13 19:58:24 +0800

    PUSH

--- src/games/prison life/155615604 - main/Utility/ACReset.lua
+++ src/games/prison life/155615604 - main/Utility/ACReset.lua
@@ -7,7 +7,7 @@ ACReset = vape.Categories.Utility:CreateModule({
 	Function = function(callback)
 		if callback then
 			local t = lplr.Team and game:GetService("Teams"):FindFirstChild(lplr.Team.Name)
-			lplr.Character.Died:Connect(function()
+			lplr.Character.Humanoid.Died:Connect(function()
 				if t then reqteam:InvokeServer(t, 1) end
 			end)
 		end
