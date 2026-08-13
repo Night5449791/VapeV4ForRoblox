cf9587729be41d585e57c7a104cffa4af9c1fe1f
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-13 19:44:02 +0800

    push

--- src/games/prison life/155615604 - main/Utility/AutoReset.lua
+++ src/games/prison life/155615604 - main/Utility/AutoReset.lua
@@ -6,17 +6,8 @@ AutoReset = vape.Categories.Utility:CreateModule({
 	Name = 'AutoReset',
 	Function = function(callback)
 		if callback then
-			local function bind(char)
-				table.insert(conns, char:WaitForChild("Humanoid").Died:Connect(function()
-					local t = lplr.Team and game:GetService("Teams"):FindFirstChild(lplr.Team.Name)
-					if t then reqteam:InvokeServer(t, 1) end
-				end))
-			end
-			table.insert(conns, lplr.CharacterAdded:Connect(bind))
-			if lplr.Character then bind(lplr.Character) end
-		else
-			for _, c in ipairs(conns) do c:Disconnect() end
-			table.clear(conns)
+			local t = lplr.Team and game:GetService("Teams"):FindFirstChild(lplr.Team.Name)
+			if t then reqteam:InvokeServer(t, 1) end
 		end
 	end,
 	Tooltip = 'Automatically switch team when lplr is dead.'
