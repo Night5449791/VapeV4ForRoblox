bb14f746d2ccab20a11090661d68be2662849206
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-15 00:36:17 +0800

    push cheaterdb

--- src/games/universal - base/Utility/CheaterDetector.lua
+++ src/games/universal - base/Utility/CheaterDetector.lua
@@ -1,4 +1,3 @@
--- i vibecoded this enjoy
 -- we all code for shits lol
 
 local CheaterDetector
@@ -9,22 +8,25 @@ local cUsernames = {
 	['WyRaff'] = 'speedhack,teleporting', -- vc server common
 	['Bonjour394'] = 'skid thinks hes powerful', -- hes js a jerk
 	['princeofegypt'] = 'skid, gets kicked for fling attempt', -- imagine gets kicked for script that kicks
-	['Chill_baconr00'] = 'skid, highjump', -- skid using vape v4 from 7granddadpgn and cant beat me XD
+	
 	['PraiseDracc'] = 'known exploiter', -- since he is commonly in vc server
 	['jerry_plsnoban7'] = 'skid,fling and got kicked', -- cringe
 	['jerry_plsnoban6'] = 'skid alt acc,fling and got kicked',
 	['jerry_plsnoban5'] = 'skid alt acc,fling and got kicked',
-	['PrisonLife_UberDrive'] = 'uber, vfly user', -- hes chilling added for vfly
-	['HeyiamTheCooolest'] = 'worst hack, slimed w/ vape v4',
+	
 	['rudeeis_ab'] = 'noclipping ahhh hack', -- saint member, dont they even use the same thing
 	['centipedeinmyheads'] = 'aimbot, wallbanging', -- another saint member lol
 	['dannielll51'] = 'headsit rip', -- inspired, vape antiheadsit soon.
-	['1267_isevil'] = 'skid, failed fling attempt',
-	['1987_isevil'] = 'skid, failed fling attempt',
 	['JOJI12416'] = 'known exploiter', -- kerax if u wonder
-	['jbskjbg'] = 'platform stand exp',
 	['BestCode_BaconThx'] = 'fling attempt',  -- ac mod in .gg/prisonlife
 	['RazhulanDeveloper'] = 'fling attempt, not a skid but he does that', -- respect, ac mod
+	--skids
+	['jbskjbg'] = 'platform stand exp',
+	['1267_isevil'] = 'skid, failed fling attempt',
+	['1987_isevil'] = 'skid, failed fling attempt',
+	['HeyiamTheCooolest'] = 'worst hack, slimed w/ vape v4',
+	['Chill_baconr00'] = 'skid, highjump', -- skid using vape v4 from 7granddadpgn and cant beat me XD
+	['PrisonLife_UberDrive'] = 'uber, vfly user', -- hes chilling added for vfly
 }
 
 local function playerAdded(plr)
