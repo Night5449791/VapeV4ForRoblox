df43a02fe5b963cdd30a347c3249c16d5723fc63
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-15 17:51:15 +0800

    upload

--- src/games/universal - base/Utility/CheaterDetector.lua
+++ src/games/universal - base/Utility/CheaterDetector.lua
@@ -28,7 +28,6 @@ local cUsernames = {
 	['princeofegypt'] = 'skid, gets kicked for fling attempt', -- imagine gets kicked for script that kicks
 	['bilinmez4095'] = 'skid, platform stand fly',
 	['djdjdd54321'] = 'skid, noclipping into walls',
-	['LaylaPowerGalaxy'] = 'test',
 }
 
 local function playerAdded(plr)
