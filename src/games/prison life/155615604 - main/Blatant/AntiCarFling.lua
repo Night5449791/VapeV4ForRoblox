ac4109549ce00de0aab5260a15c3e198adc330f8
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-15 18:34:42 +0800

    push

--- src/games/prison life/155615604 - main/Blatant/AntiCarFling.lua
+++ src/games/prison life/155615604 - main/Blatant/AntiCarFling.lua
@@ -3,7 +3,9 @@ AntiCarFling = vape.Categories.Blatant:CreateModule({
 	Name = 'AntiCarFling',
 	Function = function(callback)
 		if callback then
-			game.Workspace.CarContainer:Destroy()
+			if workspace:FindFirstChild('CarContainer') then
+				game.Workspace.CarContainer:Destroy()
+			end
             notif('AntiCarFling', 'Deleted all cars, reinject will cause bugs.', 5, 'alert')
 			AntiCarFling:Toggle()
 		end
