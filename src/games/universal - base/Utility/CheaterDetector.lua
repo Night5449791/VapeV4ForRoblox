82b9315123baab1de5d77fa28e586453a22d5598
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-15 17:24:11 +0800

    push

--- src/games/universal - base/Utility/CheaterDetector.lua
+++ src/games/universal - base/Utility/CheaterDetector.lua
@@ -6,28 +6,27 @@ local Users
 
 local cUsernames = {
 	['WyRaff'] = 'speedhack,teleporting', -- vc server common
-	['Bonjour394'] = 'skid thinks hes powerful', -- hes js a jerk
-	['princeofegypt'] = 'skid, gets kicked for fling attempt', -- imagine gets kicked for script that kicks
-	
 	['PraiseDracc'] = 'known exploiter', -- since he is commonly in vc server
-	['jerry_plsnoban7'] = 'skid,fling and got kicked', -- cringe
-	['jerry_plsnoban6'] = 'skid alt acc,fling and got kicked',
-	['jerry_plsnoban5'] = 'skid alt acc,fling and got kicked',
-	
+	['jerry_plsnoban7'] = 'fling and got kicked', -- cringe
+	['jerry_plsnoban6'] = 'fling and got kicked',
+	['jerry_plsnoban5'] = 'fling and got kicked',
 	['rudeeis_ab'] = 'noclipping ahhh hack', -- saint member, dont they even use the same thing
 	['centipedeinmyheads'] = 'aimbot, wallbanging', -- another saint member lol
-	['dannielll51'] = 'headsit rip', -- inspired, vape antiheadsit soon.
 	['JOJI12416'] = 'known exploiter', -- kerax if u wonder
 	['BestCode_BaconThx'] = 'fling attempt, ac mod',  -- ac mod in .gg/prisonlife
 	['RazhulanDeveloper'] = 'fling attempt, not a skid but he does that', -- respect, ac mod
 	--skids
-	['jbskjbg'] = 'platform stand exp',
+	['jbskjbg'] = 'skid,platform stand exp',
 	['1267_isevil'] = 'skid, failed fling attempt',
 	['1987_isevil'] = 'skid, failed fling attempt',
 	['HeyiamTheCooolest'] = 'skid, slimed w/ vape v4',
 	['Chill_baconr00'] = 'skid, highjump', -- skid using vape v4 from 7granddadpgn and cant beat me XD
 	['PrisonLife_UberDrive'] = 'skid, vfly user', -- hes chilling added for vfly
-	['gcfhjfjf4'] = 'skid, highjump . aimbot'
+	['gcfhjfjf4'] = 'skid, highjump . aimbot',
+	['dannielll51'] = 'headsit rip', -- inspired, vape antiheadsit soon.
+	['Bonjour394'] = 'skid thinks hes powerful', -- hes js a jerk
+	['princeofegypt'] = 'skid, gets kicked for fling attempt', -- imagine gets kicked for script that kicks
+	['LaylaPowerGalaxy'] = 'test account for -- cap',
 }
 
 local function playerAdded(plr)
