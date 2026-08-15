b7930bc16e37d00140ae51f903a7e35bdb4019cb
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-15 18:17:23 +0800

    upload

--- src/games/prison life/155615604 - main/Blatant/AntiCarFling.lua
+++ src/games/prison life/155615604 - main/Blatant/AntiCarFling.lua
@@ -1,12 +1,11 @@
 local AntiCarFling
-
 AntiCarFling = vape.Categories.Blatant:CreateModule({
 	Name = 'AntiCarFling',
 	Function = function(callback)
 		if callback then
+			AntiCarFling:Toggle()
 			game.Workspace.CarContainer:Destroy()
             notif('AntiCarFling', 'Deleted all cars, rejoin to fucking see shits.', 5, 'alert')
-            AntiCarFling:Toggle()
 		end
 	end,
 	Tooltip = 'just prevents u getting fucked by cars'
