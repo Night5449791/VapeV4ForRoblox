c19a211161609970da83879f1cba313b66f09678
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-15 17:29:37 +0800

    f

--- src/games/prison life/155615604 - main/Utility/AutoToxic.lua
+++ src/games/prison life/155615604 - main/Utility/AutoToxic.lua
@@ -32,10 +32,7 @@ AutoToxic = vape.Categories.Utility:CreateModule({
 	Function = function(callback)
 		if callback then
 			AutoToxic:Clean(vapeEvents.CheaterKicked.Event:Connect(function(plr)
-				-- hey some niggas help me code at dis part plz
-				if not plr == lplr then
-					sendMessage('Kicked', plr, 'skill issue | <obj>')
-				end
+				sendMessage('Kicked', plr, 'skill issue | <obj>')
 			end))
 		end
 	end,
