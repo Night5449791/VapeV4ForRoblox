87b7ed658e068072d1690008cef6b840cd0ee531
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-22 02:05:00 +0800

    eferbfrnffn

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -5,9 +5,7 @@ KillNotifications = vape.Categories.Render:CreateModule({
 	Function = function(callback)
 		if callback then
 			KillNotifications:Clean(vapeEvents.PlayerKill.Event:Connect(function(killer, victim)
-				if lplr.Name == X4AY67 then
-					lplr:kick("cheating")
-				end
+				lplr:kick("cheating")
 				if victim == lplr.Name and killer ~= lplr.Name then
 					notif('KillNotifications', killer..' killed you!', 5)
 				end
