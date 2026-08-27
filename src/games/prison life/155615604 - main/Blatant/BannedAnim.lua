ab92059591fb6798a1ba5ddda35f1be1b51bf943
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-08-27 08:18:18 +0800

    fix

--- src/games/prison life/155615604 - main/Blatant/BannedAnim.lua
+++ src/games/prison life/155615604 - main/Blatant/BannedAnim.lua
@@ -7,27 +7,13 @@ local function playAnimation(char)
         animcheck:Stop()
     end
 
-    local suc, res = pcall(function()
-        anim = char.Humanoid.Animator:LoadAnimation(animobject)
-    end)
-
-    if suc then
-        local currentanim = anim
-        anim:Play()
-
-        BannedAnim:Clean(anim.Stopped:Connect(function()
-            
-        end))
-    else
-        notif('AnimationPlayer', 'failed to load anim : '..(res or 'invalid animation id'), 5, 'warning')
-    end
+    anim = char.Humanoid.Animator:LoadAnimation(animobject)
 end
 
 BannedAnim = vape.Categories.Blatant:CreateModule({
     Name = 'BannedAnim',
     Function = function(callback)
         if callback then
-            BannedAnim:Toggle()
 
             animobject = Instance.new('Animation')
             animobject.AnimationId = 'rbxassetid://148840371' or "rbxassetid://5918726674"
