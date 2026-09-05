18a217d1bd4659e2aed70f0275c873f8435ae32d
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-06 04:49:04 +0800

    fiz

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -4,7 +4,6 @@ KillNotifications = vape.Categories.Render:CreateModule({
 	Name = 'KillNotifications',
 	Function = function(callback)
 		if callback then
-
 			KillNotifications:Clean(vapeEvents.PlayerKill.Event:Connect(function(killer, victim)
 				if victim == lplr.Name and killer ~= lplr.Name then
 					notif('KillNotifications', killer..' killed you!', 5)
