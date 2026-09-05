eaa7122b2050dc50a91597f67b11f2c1d917a620
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-06 04:16:28 +0800

    fix

--- src/games/prison life/155615604 - main/Blatant/CarFling.lua
+++ src/games/prison life/155615604 - main/Blatant/CarFling.lua
@@ -239,10 +239,12 @@ CarFling = vape.Categories.Blatant:CreateModule({
 	Name = 'CarFling',
 	Function = function(callback)
 		if not callback then
+			AntiKillPlane:Toggle()
 			resetState()
 			return
 		end
 
+		AntiKillPlane:Toggle()
 		local targetPlayer = selectedTarget()
 		local character = lplr.Character
 		local humanoid = character and character:FindFirstChildOfClass('Humanoid')
