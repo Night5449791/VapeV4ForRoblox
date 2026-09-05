b7380fe5c421bc4dcbc9f50388887a4c2516391b
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-06 05:31:50 +0800

    fix

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -4,47 +4,9 @@ KillNotifications = vape.Categories.Render:CreateModule({
 	Name = 'KillNotifications',
 	Function = function(callback)
 		if callback then
-			-- idk
-			local Player = game:GetService('Players')
-			local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-			local LocalPlayer = Players.LocalPlayer
-			local meleeEvent = ReplicatedStorage:WaitForChild('meleeEvent')
-			coroutine.wrap(function()
-				while task.wait(0.5) do
-					if AdvancedCheck.Enabled then
-						local character = LocalPlayer.Character
-						local rootPart = character and character:FindFirstChild('HumanoidRootPart')
-						if rootPart then
-							for _, player in ipairs(Players:GetPlayers()) do
-								if player ~= LocalPlayer then
-									pcall(function()
-                            			local targetCharacter = player.Character
-                            			local targetRoot = targetCharacter and targetCharacter:FindFirstChild("HumanoidRootPart")
-
-                            			local humanoid = targetCharacter and targetCharacter:FindFirstChildOfClass("Humanoid")
-
-                            			if targetRoot and humanoid and humanoid.Health > 0 then
-                                			local distance = (
-                                    			rootPart.Position - targetRoot.Position
-                                			).Magnitude
-
-                                			if distance < 10 then
-                                    			local hits = math.ceil(humanoid.Health / 5)
-
-                                    			for i = 1, hits do
-                                        			meleeEvent:FireServer(player)
-                                    			end
-											end
-                                		end
-									end)
-                            	end
-							end
-                        end
-					end
-				end
-			)()
+			-- e
 		end
+	end,
 	Tooltip = 'Sends a notification of who killed you.'
 })
 
@@ -53,3 +15,49 @@ AdvancedCheck = KillNotifications:CreateToggle({
 	Name = 'AdvancedCheck',
 	Default = false
 })
+
+--[[
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local LocalPlayer = Players.LocalPlayer
+local meleeEvent = ReplicatedStorage:WaitForChild("meleeEvent")
+
+coroutine.wrap(function()
+    while task.wait(0.5) do
+        if  then
+            local character = LocalPlayer.Character
+            local rootPart = character and character:FindFirstChild("HumanoidRootPart")
+
+            if rootPart then
+                for _, player in ipairs(Players:GetPlayers()) do
+                    if player ~= LocalPlayer then
+                        pcall(function()
+                            local targetCharacter = player.Character
+                            local targetRoot = targetCharacter
+                                and targetCharacter:FindFirstChild("HumanoidRootPart")
+
+                            local humanoid = targetCharacter
+                                and targetCharacter:FindFirstChildOfClass("Humanoid")
+
+                            if targetRoot and humanoid and humanoid.Health > 0 then
+                                local distance = (
+                                    rootPart.Position - targetRoot.Position
+                                ).Magnitude
+
+                                if distance < 10 then
+                                    local hits = math.ceil(humanoid.Health / 5)
+
+                                    for i = 1, hits do
+                                        meleeEvent:FireServer(player)
+                                    end
+                                end
+                            end
+                        end)
+                    end
+                end
+            end
+        end
+    end
+end)()
+]]
