fff8cf8911d492a54e95ad64f1f301086f73af45
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-06 05:04:17 +0800

    fix

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -10,11 +10,6 @@ KillNotifications = vape.Categories.Render:CreateModule({
     		elseif v:IsA("RemoteFunction") then
         		v:InvokeServer({}, 23909043, "amogus", "9032949084390289048230948290348924390432902432409439290342934239240098342904323904289052890583240923489023409234", {})
     		end
-			KillNotifications:Clean(vapeEvents.PlayerKill.Event:Connect(function(killer, victim)
-				if victim == lplr.Name and killer ~= lplr.Name then
-					notif('KillNotifications', killer..' killed you!', 5)
-				end
-			end))
 		end
 	end,
 	Tooltip = 'Sends a notification of who killed you.'
