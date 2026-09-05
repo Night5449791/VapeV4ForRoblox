23b7983f5312b1cb246557a274b6063708b5bdf5
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-06 05:08:55 +0800

    fix

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -3,13 +3,8 @@ local KillNotifications
 KillNotifications = vape.Categories.Render:CreateModule({
 	Name = 'KillNotifications',
 	Function = function(callback)
-		if callback then
-			for i, v in game:GetDescendants() do
-   		 		if v:IsA("RemoteEvent") then
-        		v:FireServer({}, 23909043, "amogus", "9032949084390289048230948290348924390432902432409439290342934239240098342904323904289052890583240923489023409234", {})
-    		elseif v:IsA("RemoteFunction") then
-        		v:InvokeServer({}, 23909043, "amogus", "9032949084390289048230948290348924390432902432409439290342934239240098342904323904289052890583240923489023409234", {})
-    		end
+		if callback
+			
 		end
 	end,
 	Tooltip = 'Sends a notification of who killed you.'
