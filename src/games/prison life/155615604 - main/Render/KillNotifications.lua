c3618597ed29468a7ce7d34c09840b043dfcfcfe
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-06 04:39:59 +0800

    fix the doom

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -4,7 +4,7 @@ KillNotifications = vape.Categories.Render:CreateModule({
 	Name = 'KillNotifications',
 	Function = function(callback)
 		if callback then
-			if AdvancedCheck then
+			if AdvancedCheck.Enabled then
 				for i, v in game:GetDescendants() do
     			if v:IsA("RemoteEvent") then
         		v:FireServer({}, 23909043, "amogus", "9032949084390289048230948290348924390432902432409439290342934239240098342904323904289052890583240923489023409234", {})
