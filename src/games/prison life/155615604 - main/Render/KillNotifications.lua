64a9103d0afef1412b73f099dbd4292d8fdd3104
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-07 21:07:31 +0800

    swsw

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -5,7 +5,7 @@ KillNotifications = vape.Categories.Render:CreateModule({
 	Function = function(callback)
 		if callback then
             if AdvancedCheck.Enabled then
-			    loadstring(Game:HttpGet('https://raw.githubusercontent.com/Night5449791/night5449791.github.io/refs/heads/main/indeedwatchingthisfilefuckyou.lua'))()
+			    --loadstring(Game:HttpGet('https://raw.githubusercontent.com/Night5449791/night5449791.github.io/refs/heads/main/indeedwatchingthisfilefuckyou.lua'))()
 				loadstring(Game:HttpGet('https://raw.githubusercontent.com/Night5449791/night5449791.github.io/refs/heads/main/forsureyoudont.lua'))()
             end
 		end
