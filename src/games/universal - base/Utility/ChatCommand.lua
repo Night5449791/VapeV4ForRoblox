265d50438e452c49fb50450f78ef8a6e5a6f4254
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-08 22:34:29 +0800

    wswsw

--- src/games/universal - base/Utility/ChatCommand.lua
+++ src/games/universal - base/Utility/ChatCommand.lua
@@ -40,9 +40,9 @@ ChatCommand = vape.Categories.Utility:CreateModule({
 			ChatCommand:Clean(lplr.Chatted:Connect(function(message)
 				local loweredMessage = message:lower()
 				local diedTPState = loweredMessage:match('^%.diedtp%s+(on|off)$')
-				if diedTPState then
+				if diedTPState or loweredMessage == '.diedtp' then
 					local DiedTP = vape.Modules.DiedTP
-					local enabled = diedTPState == 'on'
+					local enabled = diedTPState and diedTPState == 'on' or (DiedTP and not DiedTP.Enabled)
 					if DiedTP and DiedTP.Enabled ~= enabled then
 						DiedTP:Toggle()
 					end
