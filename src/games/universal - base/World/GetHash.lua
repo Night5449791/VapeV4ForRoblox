bc5b1aa033973ccb2759f1ecbecc20bd5c36a0e3
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-10 20:49:28 +0800

    wswswsww

--- src/games/universal - base/World/GetHash.lua
+++ src/games/universal - base/World/GetHash.lua
@@ -5,7 +5,7 @@ GetHash = vape.Categories.World:CreateModule({
 	Function = function(callback)
 		if callback then
             local data = lplr.Name..lplr.UserId
-			return hash and hash.sha512(data..'SelfReport') or ''
+			setclipboard(hash and hash.sha512(data..'SelfReport') or '')
 		end
 	end
 })
