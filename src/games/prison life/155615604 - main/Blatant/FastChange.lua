a27ca47d435e365bf6d15ac9a2214c699ede3e23
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-10 22:31:58 +0800

    fix ot delay

--- src/games/prison life/155615604 - main/Blatant/FastChange.lua
+++ src/games/prison life/155615604 - main/Blatant/FastChange.lua
@@ -7,15 +7,23 @@ FastChange = vape.Categories.Blatant:CreateModule({
     Function = function(callback)
         if callback then 
             if ChooseTeam.Value == 'Guards' then
-                task.wait(1)
-                reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Neutral"), 1)
-                task.wait(1)
-                reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Guards"), 1)
-                
+                if lplr.Team == 'Neutral' then
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Guards"), 1)
+                else
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Neutral"), 1)
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Guards"), 1)
+                end                
             elseif ChooseTeam.Value == 'Inmates' then
-                reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Neutral"), 1)
-                task.wait(1)
-                reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Inmates"), 1)
+                if lplr.Team == 'Neutral' then
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Inmates"), 1)
+                else
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Neutral"), 1)
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Inmates"), 1)
+                end   
             end
             FastChange:Toggle()
         end
