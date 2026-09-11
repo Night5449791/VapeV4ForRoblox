7ff8fcc11cd4b41ddaba8971b864bddb263fa3b5
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-12 03:13:35 +0800

    fix

--- src/games/prison life/155615604 - main/Utility/CheaterDetector.lua
+++ src/games/prison life/155615604 - main/Utility/CheaterDetector.lua
@@ -67,11 +67,13 @@ end
 
 loadLocalCheaters()
 
-local function playerAdded(plr)
+local function playerAdded(plr, notifyPlayer)
 	local username = plr and plr.Name
 	local reason = username and cheaters[username]
 	if username and type(reason) == 'string' and reason ~= '' then
-		notif('CheaterDetector', 'Cheater Detected ('..reason..'): '..plr.Name, 60, 'alert')
+		if notifyPlayer ~= false then
+			notif('CheaterDetector', 'Cheater Detected ('..reason..'): '..plr.Name, 60, 'alert')
+		end
 		whitelist.customtags[username] = {{text = 'Exploiter', color = Color3.new(1, 0, 0)}}
 		tempTargets[username] = true
 	end
@@ -80,7 +82,7 @@ end
 local function addCheater(username, reason)
 	cheaters[username] = reason
 	saveLocalCheaters()
-	playerAdded(playersService:FindFirstChild(username))
+	playerAdded(playersService:FindFirstChild(username), false)
 end
 
 local function clearCheaters()
