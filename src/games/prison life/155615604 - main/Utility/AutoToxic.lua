6189c4dab55d6159d1b3ee56d692737cd1f872b3
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-12 05:29:15 +0800

    fix auto toxic on self

--- src/games/prison life/155615604 - main/Utility/AutoToxic.lua
+++ src/games/prison life/155615604 - main/Utility/AutoToxic.lua
@@ -33,7 +33,10 @@ AutoToxic = vape.Categories.Utility:CreateModule({
 	Function = function(callback)
 		if callback then
 			AutoToxic:Clean(vapeEvents.CheaterKicked.Event:Connect(function(plr)
-				sendMessage('Kicked', plr, 'kicked <obj>| skill issue')
+    		-- 检查被踢出的玩家是否不是当前玩家
+    			if plr ~= lplr then
+        			sendMessage('Kicked', plr, 'kicked <obj>| skill issue')
+    			end
 			end))
 		end
 	end,
