19c125ec4d1596cb8c743b84a82473057c2d1845
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-14 21:02:48 +0800

    update

--- src/games/universal - base/Utility/ChatCommand.lua
+++ src/games/universal - base/Utility/ChatCommand.lua
@@ -6,7 +6,6 @@ local cServerHop
 local cReloadVape
 local cChangeTeam
 local cWhitelist
-local cTarget
 local oldCameraSubject
 local viewDeathConnection
 local replicatedStorage = game:GetService('ReplicatedStorage')
@@ -55,14 +54,6 @@ local whitelistCommands = {
 	 whitelist = true,
 	 unwl = true,
 	 unwhitelist = true
-}
-
-local targetCommands = {
-	target = true,
-	untarget = true,
-	-- idk why lol just pure fun
-	addskid = true,
-	delskid = true
 }
 
 ChatCommand = vape.Categories.Utility:CreateModule({
@@ -136,40 +127,6 @@ ChatCommand = vape.Categories.Utility:CreateModule({
 						friends:ChangeValue(player.Name)
 					end
 					notif('Whitelist', player.DisplayName..' has been whitelisted.', 5)
-				elseif loweredCommand and targetCommands[loweredCommand] and cTarget.Enabled then
-					local isBlacklist = loweredCommand == 'blacklist'
-					local target = findPlayer(prefix:match('^%s*(.-)%s*$'))
-					local player = target and target.Player
-					if not player and isBlacklist then
-						player = playersService:FindFirstChild(prefix)
-					end
-					if not player then
-						notif('Target', 'No living player found.', 5, 'warning')
-						return
-					end
-
-					local targets = vape.Categories.Targets
-					local isTargeted = table.find(targets.ListEnabled, player.Name) ~= nil
-					if isBlacklist then
-						if isTargeted then
-							targets:ChangeValue(player.Name)
-						end
-						notif('Target', player.DisplayName..' has been blacklisted.', 5)
-						return
-					elseif loweredCommand == 'untarget' then
-						if isTargeted then
-							targets:ChangeValue(player.Name)
-							notif('Target', player.DisplayName..' has been untargeted.', 5)
-						else
-							notif('Target', player.DisplayName..' is not currently targeted.', 5, 'warning')
-						end
-						return
-					end
-
-					if not isTargeted then
-						targets:ChangeValue(player.Name)
-					end
-					notif('Target', player.DisplayName..' has been targeted.', 5)
 				elseif loweredMessage == '.unview' then
 					restoreCamera()
 				elseif loweredCommand == 'tp' and cPlayerTP.Enabled then
@@ -253,10 +210,5 @@ cChangeTeam = ChatCommand:CreateToggle({
 
 cWhitelist = ChatCommand:CreateToggle({
 	Name = 'Whitelist',
-	Default = true
-})
-
-cTarget = ChatCommand:CreateToggle({
-	Name = 'Target',
 	Default = true
 })
