8ee8af37ccb8fa30b212bd451c3cfa284aca46f4
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-16 19:41:08 +0800

    update

--- src/games/prison life/155615604 - main/World/KickAll.lua
+++ src/games/prison life/155615604 - main/World/KickAll.lua
@@ -8,7 +8,7 @@ local tempList = setmetatable({}, {
 })
 
 local function getTarget(seat)
-	if tempList[seat] and tempList[seat].Health > 0 and not tempList[seat].Humanoid.Sit then
+	if tempList[seat] and tempList[seat].Health > 0 and not tempList[seat].Humanoid.Sit and select(2, whitelist:get(tempList[seat].Player)) then
 		return tempList[seat]
 	end
 
