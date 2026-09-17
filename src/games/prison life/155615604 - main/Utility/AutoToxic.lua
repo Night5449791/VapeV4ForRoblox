9a317bba3a0f1cdce3659bfc575ad258b542af7f
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-17 20:08:33 +0800

    updates

--- src/games/prison life/155615604 - main/Utility/AutoToxic.lua
+++ src/games/prison life/155615604 - main/Utility/AutoToxic.lua
@@ -1,5 +1,13 @@
 local AutoToxic
 local Toggles, Lists, Cloned, Presets = {}, {Kicked = {}}, {Kicked = {}}, {}
+local lines = {
+	Kicked = {
+		'hey anticheat kick me | kicked <obj>',
+		'prison life moment | kicked <obj>',
+		'i wonder why you got kicked | kicked <obj>',
+		'do you also want an antifling? | kicked <obj>',
+	}
+}
 
 local function sendMessage(name, obj, default)
 	local message = default
@@ -43,15 +51,6 @@ Toggles.Kicked = AutoToxic:CreateToggle({
 	Name = 'Kicked',
 	Default = true
 })
-
-local lines = {
-	Kicked = {
-		'hey anticheat kick me | kicked <obj>',
-		'prison life moment | kicked <obj>',
-		'i wonder why you got kicked | kicked <obj>',
-		'do you also want an antifling? | kicked <obj>',
-	}
-}
 
 pcall(function()
 	for _, group in textChatService:GetPresetsAsync().categoryGroups do
