7dd761b8fe7d6e6c84199a92d30243e7101317b8
Night5449791 <163004560+Night5449791@users.noreply.github.com>
2026-09-19 02:56:04 +0800

    fix

--- src/games/prison life/155615604 - main/Blatant/FastChange.lua
+++ src/games/prison life/155615604 - main/Blatant/FastChange.lua
@@ -1,50 +1,35 @@
 local FastChange
+local reqteam = game:GetService("ReplicatedStorage"):FindFirstChild("Remotes"):FindFirstChild("RequestTeamChange")
 local ChooseTeam
-local teamsService = game:GetService('Teams')
-
-local function clickTeamButton()
-	local gui = lplr.PlayerGui:FindFirstChild('TeamsFrame', true)
-	if gui then
-		for _, holder in gui:GetChildren() do
-			if holder.Button.AutoButtonColor then
-				firesignal(holder.Button.MouseButton1Click)
-				return true
-			end
-		end
-	end
-	return false
-end
 
 FastChange = vape.Categories.Blatant:CreateModule({
     Name = 'FastChange',
     Function = function(callback)
-        if callback then
-            task.spawn(function()
-                local remotes = replicatedStorage:FindFirstChild('Remotes')
-                local reqteam = remotes and remotes:FindFirstChild('RequestTeamChange')
-                local neutral = teamsService:FindFirstChild('Neutral')
-                if lplr.Team ~= neutral then
-                    if reqteam and neutral then
-                        reqteam:InvokeServer(neutral, 1)
-                    end
-                    task.wait(1.5)
-                end
-
-                if not clickTeamButton() then
-                    if reqteam then
-                        reqteam:InvokeServer(teamsService:FindFirstChild(ChooseTeam.Value), 1)
-                    else
-                        notif('FastChange', 'Team button not found.', 5, 'warning')
-                    end
-                end
-
-                if FastChange.Enabled then
-                    FastChange:Toggle()
-                end
-            end)
+        if callback then 
+            if ChooseTeam.Value == 'Guards' then
+                if lplr.Team == 'Neutral' then
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Guards"), 1)
+                else
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Neutral"), 1)
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Guards"), 1)
+                end                
+            elseif ChooseTeam.Value == 'Inmates' then
+                if lplr.Team == 'Neutral' then
+                    notif('FastChange', 'wait 2s for fadeGui', 2, 'warn')
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Inmates"), 1)
+                else
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Neutral"), 1)
+                    task.wait(1)
+                    reqteam:InvokeServer(game:GetService("Teams"):FindFirstChild("Inmates"), 1)
+                end   
+            end
+            FastChange:Toggle()
         end
     end,
-    Tooltip = 'Instantly switch team by clicking the team select GUI'
+    Tooltip = 'not-Automatically switch team'
 })
 
 ChooseTeam = FastChange:CreateDropdown({
